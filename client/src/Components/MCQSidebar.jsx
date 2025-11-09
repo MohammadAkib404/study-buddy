0e291a524f0b98798f9ad31afd92c40c0985d738
Mohammad Akib <akibo7394@gmail.com>
2025-11-09 20:14:42 +0530

    Used backend routes to get titles and displayed it.

--- client/src/Components/MCQSidebar.jsx
+++ client/src/Components/MCQSidebar.jsx
@@ -1,15 +1,26 @@
+import axios from 'axios';
 import { Bell, Book, Layers, Mouse, Package, Search } from 'lucide-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function MCQSidebar() {
 
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [titles, setTitles] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`http://localhost:5000/api/names`);
+            console.log(res.data);
+            setTitles(res.data);
+        })();
+    }, [])
 
     return (
         <>
             <div className={`bg-base p-5 fixed top-0 left-0 pt-20 border-r border-border  h-screen transition-all ease-out duration-400 ${sidebarOpen ? 'w-64 shadow-xl' : 'w-15 bg-transparent border-none shadow-none'}`}>
-                <div className='bg-brand p-3 rounded-md text-white absolute top-18 right-2' onClick={() => setSidebarOpen(prev => !prev)}><Mouse/></div>
-                <div className={`space-y-6 transition-all duration-1000 ${sidebarOpen? 'opacity-100 block': 'hidden opacity-0'}`}>
+                <div className='bg-brand p-3 rounded-md text-white absolute top-18 right-2' onClick={() => setSidebarOpen(prev => !prev)}><Mouse /></div>
+                <div className={`space-y-6 transition-all duration-1000 ${sidebarOpen ? 'opacity-100 block' : 'hidden opacity-0'}`}>
                     <div className='flex items-center gap-3'>
                         <Package />
                         <h2 className='font-bold text-lg'>Saved MCQ's</h2>
@@ -34,11 +45,16 @@ function MCQSidebar() {
                     </div>
                     <hr />
                     <div className='flex flex-col gap-3'>
-                        <h3 className='text-muted font-semibold text-sm'>Questions:-</h3>
-                        <span className='font-semibold'>Science Genrated</span>
-                        <span className='font-semibold'>Science Genrated</span>
-                        <span className='font-semibold'>Science Genrated</span>
-                        <span className='font-semibold'>Science Genrated</span>
+                        <h3 className='text-muted font-semibold text-sm'>Quizzes-</h3>
+                        {titles && titles.map((title, id) => (
+                            <span
+                                key={id}
+                                className={'cursor-pointer'}
+                                onClick={() => loadMCQ(title)}
+                                >
+                                {title}
+                            </span>
+                        ))}
                     </div>
                 </div>
             </div>
