a6eeb111f515a93a64274805e62e8d460706ba92
Mohammad Akib <akibo7394@gmail.com>
2025-12-08 16:31:54 +0530

    Configured Logout controller code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -87,7 +87,21 @@ export const login = async (req, res) => {
     return res.json({success: true});
 
   } catch (error) {
-    return res.json({success: false, message: error.message})
+    return res.json({success: false, message: `Failed to log in ${error.message}`})
   }
 };
 
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production"? "none" : "strict",
+    })
+
+    return res.json({success: true, message: "Logged Out"})
+
+  } catch (error) {
+    return res.json({success: false, message: `Failed to log out: ${error.message}`})
+  }
+}
