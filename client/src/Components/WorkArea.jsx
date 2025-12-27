9792552934a797c549d4a9add1be7bf10f8e5e8a
Mohammad Akib <akibo7394@gmail.com>
2025-12-27 14:34:26 +0530

    Refactor WorkArea component to use FilesPreview and enhance file handling logic

--- client/src/Components/WorkArea.jsx
+++ client/src/Components/WorkArea.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Input from './Input'
 
 import { ArrowDown, FileText, Image } from 'lucide-react';
-import FilePreview from './FilePreview';
+import FilesPreview from './FilesPreview';
 import Attachments from './Attachments';
 
 export default function WorkArea() {
@@ -23,42 +23,46 @@ export default function WorkArea() {
   }
 
   const fileStatus = (file) => {
-  if (!file || !file.name) return null;
+    if (!file || !file.name) return null;
 
-  const parts = file.name.trim().split(".");
-  if (parts.length < 2) return null; // no extension found
+    const parts = file.name.trim().split(".");
+    if (parts.length < 2) return null; // no extension found
 
-  let name = parts.shift();
-  const MAX_LENGTH = 15;
-  if(name.length > MAX_LENGTH){
-     const namePart = name.slice(0, MAX_LENGTH - 3);
-     name = namePart + "..."
-  }
-  const ext = parts.pop().toLowerCase();
+    let name = parts.shift();
+    const MAX_LENGTH = 18;
+    if (name.length > MAX_LENGTH) {
+      const namePart = name.slice(0, MAX_LENGTH - 3);
+      name = namePart + "..."
+    }
+    const ext = parts.pop().toLowerCase();
 
-  const validTypes = ["pdf", "docx", "txt"];
-  if (!validTypes.includes(ext)) return null;
+    const validTypes = ["pdf", "docx", "txt"];
+    if (!validTypes.includes(ext)) return null;
 
-  if (file.type) {
-    if (ext === "pdf" && file.type !== "application/pdf") return null;
-    if (ext === "docx" && file.type !== "application/vnd.openxmlformats-officedocument.wordprocessingml.document") return null;
-  }
+    if (file.type) {
+      if (ext === "pdf" && file.type !== "application/pdf") return null;
+      if (ext === "docx" && file.type !== "application/vnd.openxmlformats-officedocument.wordprocessingml.document") return null;
+    }
 
-  return { name, type: ext.toUpperCase() };
-};
+    return { name, type: ext.toUpperCase() };
+  };
 
 
   const handleDrop = (e) => {
-  e.preventDefault();
-  setIsDragging(false);
+    e.preventDefault();
+    setIsDragging(false);
 
-  const files = e.dataTransfer.files
+    const files = e.dataTransfer.files
+    if (files.length < 1) return console.log("Please Upload proper file")
 
-  for (const file of files) {
-    const status = fileStatus(file);
-    if(status) setFileData(prev => [...prev, status]);
-  }
-};
+    let data = [];
+    for (const file of files) {
+      const status = fileStatus(file);
+      if (status) data.push(status);
+    }
+    console.log(data);
+    setFilesData(prev => prev? [...prev, ...data] : data);
+  };
 
 
   return (
@@ -66,27 +70,23 @@ export default function WorkArea() {
       <div className='w-full flex flex-col items-center'>
         <strong className='text-[28px] font-semibold'>What do you want to Create?</strong>
         <p className='mb-8 text-muted'>Upload your materials or type a topic to generate assessment.</p>
-        {/* <Input /> */}
+
         <div className='w-7/10 flex flex-col border border-border rounded-2xl'>
-        <div className='flex gap-3 items-center px-3 py-2'>
-         {fileData.map((data, i) => (
-          <FilePreview fileData={data}/>
-         ))}
+          {filesData.length > 0? <FilesPreview filesData={filesData} setFilesData={setFilesData} /> : ""}
+          <Input />
+          <div className='flex justify-between items-center px-3 py-1'>
+            <Attachments />
+            <ArrowDown className='bg-primary text-white rounded-full p-1 size-8' />
+          </div>
         </div>
-        <Input/>
-        <div className='flex justify-between items-center px-3 py-1'>
-          <Attachments/>
-          <ArrowDown className='bg-primary text-white rounded-full p-1 size-8' />
-        </div>
-      </div>
 
         <div className={`${isDragging ? "opacity-100 backdrop-blur-xs" : "opacity-0 backdrop-blur-none"} pointer-events-none transition-all duration-400 flex flex-col gap-4 items-center pt-[30vh] bg-base/70 pointer-events fixed left-0 top-0 z-99 w-screen h-screen`}>
           <div className='flex gap-5'>
             <Image className='size-20 bg-transparent rounded-md -rotate-10' />
             <FileText className='size-20 rotate-30 ' />
           </div>
-          <h1 className='font-bold text-4xl mt-8'>Add Anything</h1>
-          <p className='text-muted font-bold text-xl'>Drop Files Here (PDF, Docx, Text)</p>
+          <h1 className='font-bold text-4xl mt-8'>Got Documents?</h1>
+          <p className='text-muted font-bold text-xl'>Upload up to 6 files(PDF, Docx, Txt)</p>
         </div>
       </div>
     </main>
